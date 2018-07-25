5533f3d8320e67f6bbd669a20db42a0bf3150506
ChinaskiJr <robin.colombier@gmail.com>
2018-07-25 15:32:03 +0200

    catchException() now gets a developper mode

--- errors.php
+++ errors.php
@@ -1,11 +1,17 @@
 <?php
+set_error_handler('error2exception');
 /**
  * Return a beautiful string that explains to you exactly what you failed.
  * @param Exception $e The exception.
+ * @param bool $devMode If true, the exception will print more informations : type, line...
  * @return void
  */
-function catchException(Exception $e) {
-	echo 'An error occured in <strong>' . $e->getFile() . '</strong> on line <strong>' . $e->getLine() . '</strong>: <br />' . $e->getMessage(); 
+function catchException(Exception $e, $devMode) {
+	if ($devMode) {
+		echo '<strong> ' . get_class($e) . '</strong> : an error occured in <strong>' . $e->getFile() . '</strong> on line <strong>' . $e->getLine() . '</strong>: <i>' . $e->getMessage() . '</i><br />'; 
+	} else {
+		echo '<strong>' . $e->getMessage() . '</strong><br />';
+	}
 }
 /**
  * Send the paremeters of the PHP error to our MyException class
