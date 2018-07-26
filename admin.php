56c7362765ac37d664c5f9d620f739f6549440c4
ChinaskiJr <robin.colombier@gmail.com>
2018-07-26 12:05:09 +0200

    finish the frontend part

--- admin.php
+++ admin.php
@@ -1,30 +1,95 @@
 <?php
-require_once 'lib/errors.php';
-require_once 'lib/autoload.php';
-require_once 'templates/htmlHead.html';
 try {
+	require_once 'lib/errors.php';
+	require_once 'lib/autoload.php';
+	require_once 'templates/htmlHead.html';
 	require_once 'lib/dbConnect.php';
 	if (!empty($_POST)) {
 		// Check integrity of the data
 		if (!empty($_POST['author'])) {
 			if (empty($_POST['title']) || empty($_POST['content'])) {
-				throw new InvalidArgumentException('The title of the post nor the content can\'t be empty.');
+				throw new InvalidArgumentException('the title of the post nor the content can\'t be empty.');
 			}
-			$news = new News($_POST);
+			if (!isset($_POST['id'])) {
+				$news = new News($_POST);
+				$manager->add($news);
+			} else {
+				$news = $manager->get($_POST['id']);
+				$news->setAuthor($_POST['author']);
+				$news->setTitle($_POST['title']);
+				$news->setContent($_POST['content']);
+				$manager->edit($news);
+			}
 		} else {
-			throw new InvalidArgumentException('The author must have a name...');		
+			throw new InvalidArgumentException('the author must have a name...');		
 		}
-
 	}
+	if (isset($_GET['edit'])) {
+		$newsToEdit = $manager->get($_GET['edit']);
+	}
+	if (isset($_GET['delete'])) {
+		$manager->delete($_GET['delete']);
+	}
+	$nbNews = $manager->count();
+	$allNews = $manager->getList($nbNews);
 } catch (Exception $e) {
 	// set the 2nd argument on true for developper mode
 	catchException($e, true);
 }
 ?>
+
 <a href=".">Go back to the main page</a>
-<form action="admin.php" method="post" style="text-align: center;">
-	Author : <input type="text" name="author"/><br />
-	Titre : <input type="text" name="title"/><br />
-	Content :<br /><textarea name="content" cols="50" rows="10"></textarea><br />
-	<input type="submit" value="Post">
+<form action="admin.php" method="post">
+	Author : <input type="text" name="author" value="<?php if(isset($newsToEdit)) echo $newsToEdit->author(); ?>"/><br />
+	Titre : <input type="text" name="title" value="<?php if(isset($newsToEdit)) echo $newsToEdit->title(); ?>"/><br />
+	Content :<br />
+		<textarea name="content" cols="50" rows="10"><?php if(isset($newsToEdit)) echo $newsToEdit->content(); ?></textarea><br />
+	<?php 
+	if (isset($newsToEdit)) {
+		?>
+		<input type="hidden" name="id" value="<?=$newsToEdit->id()?>">
+		<input type="submit" value="Edit">
+		<?php
+	} else {
+		?>
+		<input type="submit" value="Post">
+		<?php
+	}
+	?>
 </form>
+
+<section class="editNews">
+	<p>There is actually <?=$nbNews?> news. Here they are :</p>
+	<table>
+		<tr class="firstLineTable">
+			<th>Author</th>
+			<th>Title</th>
+			<th>Date of creation</th>
+			<th>Date of last edition</th>
+			<th>Action</th>
+		</tr>
+		<?php
+		foreach ($allNews as $key => $value) {
+			?>
+			<tr class="contentTable">
+				<td><?=htmlspecialchars($value->author())?></td>
+				<td><?=htmlspecialchars($value->title())?></td>
+				<td><?=$value->dateAdd()->format("Y-m-d à H\hi")?></td>
+				<?php
+				if ($value->dateAdd() != $value->dateEdit()) {
+					?>
+					<td><?=$value->dateEdit()->format("Y-m-d à H\hi")?></td>
+					<?php
+				} else {
+					echo '<td>-</td>';
+				}
+				?>
+				<td><a href="?edit=<?=$value->id()?>">Edit</a> | <a href="?delete=<?=$value->id()?>">Delete</a></td>
+			</tr>
+			<?php
+		}
+		?>
+	</table>
+</section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,11 +2,38 @@
 require_once 'lib/errors.php';
 require_once 'lib/autoload.php';
 require_once 'templates/htmlHead.html';
-require_once 'lib/dbConnect.php';
+try {
+	require_once 'lib/dbConnect.php';
+	$fiveLastNews = $manager->getList(5);
+} catch (Exception $e) {
+	catchException($e, true);
+}
 ?>
 
 <body>
 	<a href="admin.php">Access to the administration page</a>
 	<h1>List of the five last news</h1>
+	<?php
+		foreach ($fiveLastNews as $value) {
+			?>
+			<section class="news">
+				<h2><?=htmlspecialchars($value->title())?></h2>
+				<i>Écrit par <?=htmlspecialchars($value->author())?></i>
+				<p><?=htmlspecialchars($value->content())?></p>
+				<p class="date">
+					<span>Posté le <?=$value->dateAdd()->format("Y-m-d à H\hi")?></span>
+					<?php 
+					// If dateEdit is different, then we print it
+					if ($value->dateAdd() != $value->dateEdit()) {
+						?>
+						<span>Édité le <?=$value->dateEdit()->format("Y-m-d à H\hi")?></span>
+						<?php 
+					}
+					?>
+				</p>
+			</section>
+			<?php
+		}
+	?>
 </body>
 </html>
